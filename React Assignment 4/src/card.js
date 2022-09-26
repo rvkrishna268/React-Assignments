7e672f2dc689fbe4a145377229c19a324b66b88a
rvkrishna268 <rvkrishna268@gmail.com>
2022-09-26 19:50:19 +0530

    Added React Assignment 4 files

---
+++ React Assignment 4/src/card.js
@@ -0,0 +1,39 @@
+import "./card.css";
+const Card = ({post}) => {
+    return(
+        <>
+        <section className="card">
+            <section className="card-header">
+                <div>
+                    <div className="card-header-name">{post.name}</div>
+                    <div className="card-header-place">{post.location}</div>
+                </div>
+                <span>
+                    <i className="fa fa-ellipsis-h" aria-hidden="true"></i>
+                </span>
+            </section>
+            <section className="card-image">
+                <img src={require("" + post.PostImage)} alt="place" />
+            </section>
+            <section className="card-actions"> 
+                <span>
+                <i class="fa fa-heart" aria-hidden="true"></i>  
+                              </span>
+                <span>
+                <i class="fa fa-paper-plane" aria-hidden="true"></i>
+                </span>
+                <span>
+                    {post.date}
+                </span>
+            </section>
+            <section className="likes">
+                {post.likes} likes
+            </section>
+            <section className="description">
+                {post.description} 
+            </section>
+        </section>
+    </>
+    )
+}
+export default Card;
